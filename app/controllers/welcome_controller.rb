87894a58bd7fcd1ffae0ff6b0e4b810c3abcae66
t.kostiuchenko <t.kostiuchenko@111minutes.com>
2014-10-31 10:54:24 +0200

    zero project

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  def index3
+  def index
   end
   def hello
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
   root 'welcome#index'
-	get  '/bbb' => 'welcome#hello'
   
 end
